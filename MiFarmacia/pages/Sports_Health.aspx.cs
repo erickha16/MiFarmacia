1e93c73b5eff77b9e9fa2829b289b1a6e68bc624
erickha16 <errickha@gmail.com>
2025-06-05 01:52:00 -0600

    Imagens dinámicas en clientes

--- MiFarmacia/pages/Sports_Health.aspx.cs
+++ MiFarmacia/pages/Sports_Health.aspx.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using CapaNegocio;
+using RutasGen6.Util;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using VO;
 
 namespace Proyecto1_Farmacia_.pages
 {
@@ -11,7 +14,25 @@ namespace Proyecto1_Farmacia_.pages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                CargarProductos();
+            }
+        }
 
+        private void CargarProductos()
+        {
+            try
+            {
+                List<ProductosVO> lista = BllProductos.GetListProductos();
+                rptProductos.DataSource = lista;
+                rptProductos.DataBind();
+            }
+            catch (Exception ex)
+            {
+                // Puedes registrar el error o mostrar un mensaje
+                UtilControls.SweetBox("Error!", ex.ToString(), "error", this.Page, this.GetType());
+            }
         }
     }
 }
--- MiFarmacia/pages/Sports_Health.aspx.designer.cs
+++ MiFarmacia/pages/Sports_Health.aspx.designer.cs
@@ -13,5 +13,14 @@ namespace Proyecto1_Farmacia_.pages
 
     public partial class Deportes_Nutricion
     {
+
+        /// <summary>
+        /// Control rptProductos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptProductos;
     }
 }
